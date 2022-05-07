a55bcd93b6c3eff2d1d4641693e840a756dca2e0
Augusto Cesar <acs-03@hotmail.com>
2022-05-07 15:34:08 -0300

    Preparing to build

--- frontend/src/app/services/transaction.service.ts
+++ frontend/src/app/services/transaction.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const baseUrl = 'https://acsousa-bankline.herokuapp.com/';
+const baseUrl = environment.API_URL;
 
 @Injectable({
   providedIn: 'root',
